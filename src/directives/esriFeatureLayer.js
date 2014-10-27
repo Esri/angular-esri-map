e4e10d3c6de1ec4e4adbc86bcd9190391a5cc8bc
Tom Wayson <tom@tomwayson.com>
2014-10-27 06:37:26 -0400

    using strict option by wrapping each file in IIFE

--- src/directives/esriFeatureLayer.js
+++ src/directives/esriFeatureLayer.js
@@ -1,55 +1,60 @@
-angular.module('esri.map').directive('esriFeatureLayer', function ($q) {
-    // this object will tell angular how our directive behaves
-    return {
-        // only allow esriFeatureLayer to be used as an element (<esri-feature-layer>)
-        restrict: 'E',
+(function(angular) {
+    'use strict';
 
-        // require the esriFeatureLayer to have its own controller as well an esriMap controller
-        // you can access these controllers in the link function
-        require: ['esriFeatureLayer', '^esriMap'],
+    angular.module('esri.map').directive('esriFeatureLayer', function ($q) {
+        // this object will tell angular how our directive behaves
+        return {
+            // only allow esriFeatureLayer to be used as an element (<esri-feature-layer>)
+            restrict: 'E',
 
-        // replace this element with our template.
-        // since we aren't declaring a template this essentially destroys the element
-        replace: true,
+            // require the esriFeatureLayer to have its own controller as well an esriMap controller
+            // you can access these controllers in the link function
+            require: ['esriFeatureLayer', '^esriMap'],
 
-        // define an interface for working with this directive
-        controller: function ($scope, $element, $attrs) {
-            var layerDeferred = $q.defer();
+            // replace this element with our template.
+            // since we aren't declaring a template this essentially destroys the element
+            replace: true,
 
-            require([
-                'esri/layers/FeatureLayer'], function (FeatureLayer) {
-                var layer = new FeatureLayer($attrs.url);
+            // define an interface for working with this directive
+            controller: function ($scope, $element, $attrs) {
+                var layerDeferred = $q.defer();
 
-                layerDeferred.resolve(layer);
-            });
+                require([
+                    'esri/layers/FeatureLayer'], function (FeatureLayer) {
+                    var layer = new FeatureLayer($attrs.url);
 
-            // return the defered that will be resolved with the feature layer
-            this.getLayer = function () {
-                return layerDeferred.promise;
-            };
-        },
-
-        // now we can link our directive to the scope, but we can also add it to the map..
-        link: function (scope, element, attrs, controllers) {
-            // controllers is now an array of the controllers from the 'require' option
-            var layerController = controllers[0];
-            var mapController = controllers[1];
-
-            layerController.getLayer().then(function (layer) {
-                // add layer
-                mapController.addLayer(layer);
-
-                //look for layerInfo related attributes. Add them to the map's layerInfos array for access in other components
-                mapController.addLayerInfo({
-                  title: attrs.title || layer.name,
-                  layer: layer,
-                  hideLayers: (attrs.hideLayers) ? attrs.hideLayers.split(',') : undefined,
-                  defaultSymbol: (attrs.defaultSymbol) ? JSON.parse(attrs.defaultSymbol) : true
+                    layerDeferred.resolve(layer);
                 });
 
-                // return the layer
-                return layer;
-            });
-        }
-    };
-});
+                // return the defered that will be resolved with the feature layer
+                this.getLayer = function () {
+                    return layerDeferred.promise;
+                };
+            },
+
+            // now we can link our directive to the scope, but we can also add it to the map..
+            link: function (scope, element, attrs, controllers) {
+                // controllers is now an array of the controllers from the 'require' option
+                var layerController = controllers[0];
+                var mapController = controllers[1];
+
+                layerController.getLayer().then(function (layer) {
+                    // add layer
+                    mapController.addLayer(layer);
+
+                    //look for layerInfo related attributes. Add them to the map's layerInfos array for access in other components
+                    mapController.addLayerInfo({
+                      title: attrs.title || layer.name,
+                      layer: layer,
+                      hideLayers: (attrs.hideLayers) ? attrs.hideLayers.split(',') : undefined,
+                      defaultSymbol: (attrs.defaultSymbol) ? JSON.parse(attrs.defaultSymbol) : true
+                    });
+
+                    // return the layer
+                    return layer;
+                });
+            }
+        };
+    });
+
+})(angular);
--- src/directives/esrilegend.js
+++ src/directives/esrilegend.js
@@ -1,53 +1,58 @@
-/**
- * @ngdoc directive
- * @name esriApp.directive:esriLegend
- * @description
- * # esriLegend
- */
-angular.module('esri.map')
-  .directive('esriLegend', function ($document, $q) {
-    return {
-      //run last
-      priority: -10,
-      scope:{},
-      replace: true,
-      // require the esriMap controller
-      // you can access these controllers in the link function
-      require: ['^esriMap'],
+(function(angular) {
+    'use strict';
 
-      // now we can link our directive to the scope, but we can also add it to the map..
-      link: function(scope, element, attrs, controllers){
-        // controllers is now an array of the controllers from the 'require' option
-        var mapController = controllers[0];
-        var targetId = attrs.targetId || attrs.id;
-        var legendDeferred = $q.defer();
+    /**
+     * @ngdoc directive
+     * @name esriApp.directive:esriLegend
+     * @description
+     * # esriLegend
+     */
+    angular.module('esri.map')
+      .directive('esriLegend', function ($document, $q) {
+        return {
+          //run last
+          priority: -10,
+          scope:{},
+          replace: true,
+          // require the esriMap controller
+          // you can access these controllers in the link function
+          require: ['^esriMap'],
 
-        require(['esri/dijit/Legend', 'dijit/registry'], function (Legend, registry) {
-          mapController.getMap().then(function(map) {
-            var opts = {
-                map: map
-            };
-            var layerInfos = mapController.getLayerInfos();
-            if (layerInfos) {
-              opts.layerInfos = layerInfos;
-            }
-            // NOTE: need to come up w/ a way to that is not based on id
-            // or handle destroy at end of this view's lifecyle
-            var legend = registry.byId(targetId);
-            if (legend) {
-              legend.destroyRecursive();
-            }
-            legend = new Legend(opts, targetId);
-            legend.startup();
-            scope.layers = legend.layers;
-            angular.forEach(scope.layers, function(layer, i) {
-              scope.$watch('layers['+i+'].renderer',function() {
-                legend.refresh();
+          // now we can link our directive to the scope, but we can also add it to the map..
+          link: function(scope, element, attrs, controllers){
+            // controllers is now an array of the controllers from the 'require' option
+            var mapController = controllers[0];
+            var targetId = attrs.targetId || attrs.id;
+            var legendDeferred = $q.defer();
+
+            require(['esri/dijit/Legend', 'dijit/registry'], function (Legend, registry) {
+              mapController.getMap().then(function(map) {
+                var opts = {
+                    map: map
+                };
+                var layerInfos = mapController.getLayerInfos();
+                if (layerInfos) {
+                  opts.layerInfos = layerInfos;
+                }
+                // NOTE: need to come up w/ a way to that is not based on id
+                // or handle destroy at end of this view's lifecyle
+                var legend = registry.byId(targetId);
+                if (legend) {
+                  legend.destroyRecursive();
+                }
+                legend = new Legend(opts, targetId);
+                legend.startup();
+                scope.layers = legend.layers;
+                angular.forEach(scope.layers, function(layer, i) {
+                  scope.$watch('layers['+i+'].renderer',function() {
+                    legend.refresh();
+                  });
+                });
+                legendDeferred.resolve(legend);
               });
             });
-            legendDeferred.resolve(legend);
-          });
-        });
-      }
-    };
-  });
+          }
+        };
+      });
+
+})(angular);
