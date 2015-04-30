816558eba1cf3c13771cf7af80379838abbfb040
willisd2 <willisd2@michigan.gov>
2015-04-30 16:53:18 -0400

    added support for a visible attribute on the esriFeatureLayer directive

--- dist/angular-esri-map.js
+++ dist/angular-esri-map.js
@@ -395,6 +395,25 @@
                 this.getLayer = function () {
                     return layerDeferred.promise;
                 };
+
+                // set the visibility of the feature layer
+                this.setVisible = function (isVisible) {
+                    var visibleDeferred = $q.defer();
+
+                    this.getLayer().then(function (layer) {
+                        if (isVisible === true || isVisible.toString().toLowerCase() === 'true') {
+                            layer.show();
+                        }
+                        else if(isVisible === false || isVisible.toString().toLowerCase() === 'false')
+                        {
+                            layer.hide();
+                        }
+
+                        visibleDeferred.resolve();
+                    });
+
+                    return visibleDeferred.promise;
+                };
             },
 
             // now we can link our directive to the scope, but we can also add it to the map..
@@ -402,6 +421,20 @@
                 // controllers is now an array of the controllers from the 'require' option
                 var layerController = controllers[0];
                 var mapController = controllers[1];
+
+                var visible = attrs['visible'] || true;
+                var isVisible = scope.$eval(visible);
+
+                // set the initial visible state of the feature layer
+                layerController.setVisible(isVisible);
+
+                // add a $watch condition on the visible attribute, if it changes and the new value is different than the previous, then use to
+                // set the visibility of the feature layer
+                scope.$watch(function () { return scope.$eval(attrs['visible']); }, function (newVal, oldVal) {
+                    if (newVal !== oldVal) {
+                        layerController.setVisible(newVal);
+                    }
+                });
 
                 layerController.getLayer().then(function (layer) {
                     // add layer
