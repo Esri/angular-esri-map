89a1e5dfab4d6ab65a471055e331b9a8e2c4c30b
Jacob Wasilkowski <jwasilkowski@esri.com>
2015-09-12 14:02:49 -0500

    added appConfig constant provider with example page template properties and routes; examples.html and app.js $routeProvider now loop over appConfig.examplePages; typo fix for protractor conf.js

--- docs/app/app.js
+++ docs/app/app.js
@@ -5,58 +5,26 @@
     // see: https://github.com/pc035860/angular-highlightjs#demos
     angular
         .module('esri-map-docs', ['ngRoute', 'ngSanitize', 'ngSelect', 'hljs', 'esri.map'])
-        .config(function($routeProvider) {
+        .config(function($routeProvider, appConfig) {
             $routeProvider
-            .when('/examples', {
-                templateUrl: 'app/examples/examples.html',
-                controller: 'ExamplesCtrl'
-            })
-            .when('/examples/simple-map', {
-                templateUrl: 'app/examples/simple-map.html',
-                controller: 'SimpleMapCtrl'
-            })
-            .when('/examples/feature-layers', {
-                templateUrl: 'app/examples/feature-layers.html',
-                controller: 'FeatureLayersCtrl'
-            })
-            .when('/examples/web-map', {
-                templateUrl: 'app/examples/web-map.html',
-                controller: 'WebMapCtrl'
-            })
-            .when('/examples/legend', {
-                templateUrl: 'app/examples/legend.html',
-                controller: 'LegendCtrl'
-            })
-            .when('/examples/center-and-zoom', {
-                templateUrl: 'app/examples/center-and-zoom.html',
-                controller: 'CenterAndZoomCtrl'
-            })
-            .when('/examples/basemap', {
-                templateUrl: 'app/examples/basemap.html',
-                controller: 'BasemapCtrl'
-            })
-            .when('/examples/map-events', {
-                templateUrl: 'app/examples/map-events.html',
-                controller: 'MapEventsCtrl'
-            })
-            .when('/examples/registry-pattern', {
-                templateUrl: 'app/examples/registry-pattern.html',
-                controller: 'RegistryPatternCtrl'
-            })
-            .when('/examples/additional-map-options', {
-                templateUrl: 'app/examples/additional-map-options.html',
-                controller: 'AdditionalMapOptionsCtrl'
-            })
-            .when('/examples/no-basemap', {
-                templateUrl: 'app/examples/no-basemap.html',
-                controller: 'NoBasemapCtrl'
-            })
-            .when('/about', {
-                templateUrl: 'app/about/about.html',
-                controller: 'AboutCtrl'
-            })
-            .otherwise({
-                redirectTo: '/examples'
+                .when('/examples', {
+                    templateUrl: 'app/examples/examples.html',
+                    controller: 'ExamplesCtrl'
+                })
+                .when('/about', {
+                    templateUrl: 'app/about/about.html',
+                    controller: 'AboutCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/examples'
+                });
+            // set routes of examples pages from appConfig
+            angular.forEach(appConfig.examplePages, function(example) {
+                $routeProvider
+                    .when(example.route.path, {
+                        templateUrl: example.route.templateUrl,
+                        controller: example.route.controller
+                    });
             });
         });
 })(angular);
