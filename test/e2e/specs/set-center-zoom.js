e03423ce17af4dc6c3608ef43c56627ac464d91e
Jacob Wasilkowski <jwasilkowski@esri.com>
2015-09-13 16:02:49 -0500

    new integration test for map-events; updated test/set-center-zoom with some additional clicks to help it play more nicely; resolves issue #

--- test/e2e/specs/set-center-zoom.js
+++ test/e2e/specs/set-center-zoom.js
@@ -5,6 +5,7 @@ var helper = require('../helper');
 describe('Set Map Center and Zoom', function() {
     // shared element locators
     var map = element(by.id('map'));
+    var zoomIn = element(by.css('.esriSimpleSliderIncrementButton'));
     var lat = element(by.model('map.center.lat'));
     var lng = element(by.model('map.center.lng'));
     var zoomSelect = element(by.model('map.zoom'));
@@ -17,8 +18,10 @@ describe('Set Map Center and Zoom', function() {
     it('should click on the "San Francisco" button and change the map "data-zoom" value, along with the lat, lng, and zoom element values', function() {
         // element locator(s) specific to this test
         var sanFrancisco = element(by.buttonText('San Francisco'));
+        helper.waitUntilElementIsReady(zoomIn);
         helper.waitUntilElementIsReady(map);
 
+        zoomIn.click(); // clicking on this first seems to help successfully execute a click on the following button
         sanFrancisco.click();
 
         helper.getAsyncAttributeValue(map, 'data-zoom').then(function(newValue) {
@@ -35,8 +38,10 @@ describe('Set Map Center and Zoom', function() {
     it('should click on the "New York" button and change the map "data-zoom" value, along with the lat, lng, and zoom element values', function() {
         // element locator(s) specific to this test
         var newYork = element(by.buttonText('New York'));
+        helper.waitUntilElementIsReady(zoomIn);
         helper.waitUntilElementIsReady(map);
 
+        zoomIn.click(); // clicking on this first seems to help successfully execute a click on the following button
         newYork.click();
 
         helper.getAsyncAttributeValue(map, 'data-zoom').then(function(newValue) {
