6ac67f18ca0a47c411705a46e53472c64c163e80
Amanda Myer <mander77@gmail.com>
2015-10-12 09:00:13 -0400

    Cleaned up esriBasemap.js

--- src/directives/esriBasemap.js
+++ src/directives/esriBasemap.js
@@ -4,30 +4,21 @@
     angular.module('esri.map').directive('esriBasemap', function (esriRegistry) {
         // this object will tell angular how our directive behaves
         return {
-            // only allow esriIdentifyTask to be used as an element (<esri-identify-task>)
+            // only allow esriBasemap to be used as an element (<esri-identify-task>)
             restrict: 'E',
 
-            // require the esriBasemap to have its own controller as well an esriMap controller
+            // require the esriBasemap to have an esriMap controller
             // you can access these controllers in the link function
-            require: ['esriBasemap', '^esriMap'],
+            require: ['^esriMap'],
 
             // replace this element with our template.
             // since we aren't declaring a template this essentially destroys the element
             replace: true,
 
-            // define an interface for working with this directive
-            controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
-                var self = this;
-                self.map = null;
-                self.Basemaps = [];
-                
-            }],
-
             // now we can link our directive to the scope, but we can also add it to the map..
             link: function (scope, element, attrs, controllers) {
                 // controllers is now an array of the controllers from the 'require' option
-                var basemapController = controllers[0];
-                var mapController = controllers[1];
+                var mapController = controllers[0];
 
                 var urlArray = attrs.urls.split(",");
                 
