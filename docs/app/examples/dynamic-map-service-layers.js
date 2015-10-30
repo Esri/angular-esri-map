05b2d12cedc905d614e9bf729835a49cfbf00cc8
Tom Wayson <tom@tomwayson.com>
2015-10-29 22:44:26 -0700

    updated to be compatible w/ #133
    use registry to get map and set basemap after custom basemap is added
    only load and set infoWindow symbols after map is loaded

--- docs/app/examples/dynamic-map-service-layers.js
+++ docs/app/examples/dynamic-map-service-layers.js
@@ -8,43 +8,21 @@ angular.module('esri-map-docs')
             '<div class="demographicNumericPadding">${AGE_65_UP:formatNumber}</div><div class="demographicInnerSpacing"></div>people ages 65 and older' +
             '</div>';
 
-        // functions used in popup templates must have a global scope
+        // functions used in popup templates must have a global scope :(
         window.formatNumber = function(value /*, key, data*/ ) {
             var searchText = '' + value;
             var formattedString = searchText.replace(/(\d)(?=(\d\d\d)+(?!\d))/gm, '$1,');
             return formattedString;
         };
 
-        esriLoader.require([
-            'esri/Color',
-            'esri/dijit/Popup',
-            'esri/symbols/SimpleFillSymbol',
-            'esri/symbols/SimpleLineSymbol'
-        ]).then(function(modules) {
-            var Color = modules[0];
-            var Popup = modules[1];
-            var SimpleFillSymbol = modules[2];
-            var SimpleLineSymbol = modules[3];
-
-            var sfs = new SimpleFillSymbol(
-                'solid',
-                new SimpleLineSymbol('solid', new Color('#444444'), 3),
-                new Color([68, 68, 68, 0.25]));
-
-            var popup = new Popup({
-                fillSymbol: sfs
-            }, angular.element('<div></div>')[0]);
-
-            $scope.map = {
-                options: {
-                    basemap: 'topo',
-                    center: [-94.75290067627297, 39.034671990514816], // long, lat
-                    zoom: 12,
-                    sliderStyle: 'small',
-                    infoWindow: popup
-                }
-            };
-        });
+        $scope.map = {
+            options: {
+                basemap: 'topo',
+                center: [-94.75290067627297, 39.034671990514816], // long, lat
+                zoom: 12,
+                sliderStyle: 'small'
+            }
+        };
 
         $scope.demographicsLayer = {
             url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer',
@@ -67,5 +45,26 @@ angular.module('esri-map-docs')
                     }
                 }
             }
+        };
+
+        // this sample uses a custom info window
+        $scope.setInfoWindow = function(map) {
+            // load the popup and symbols needed
+            esriLoader.require([
+                'esri/Color',
+                'esri/symbols/SimpleFillSymbol',
+                'esri/symbols/SimpleLineSymbol'
+            ]).then(function(modules) {
+                var Color = modules[0];
+                var SimpleFillSymbol = modules[1];
+                var SimpleLineSymbol = modules[2];
+
+                var sfs = new SimpleFillSymbol(
+                    'solid',
+                    new SimpleLineSymbol('solid', new Color('#444444'), 3),
+                    new Color([68, 68, 68, 0.25]));
+
+                map.infoWindow.set('fillSymbol', sfs);
+            });
         };
     });
