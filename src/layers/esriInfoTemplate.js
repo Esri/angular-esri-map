bbd5283b0c5b720d719faadb1b78937b3eefab00
Jacob Wasilkowski <jwasilkowski@esri.com>
2015-11-14 18:26:00 -0600

    docs for esriInfoTemplate

--- src/layers/esriInfoTemplate.js
+++ src/layers/esriInfoTemplate.js
@@ -1,6 +1,37 @@
 (function (angular) {
     'use strict';
 
+    /**
+     * @ngdoc directive
+     * @name esri.map.directive:esriInfoTemplate
+     * @restrict E
+     * @element
+     *
+     * @description
+     * This directive creates an
+     * {@link https://developers.arcgis.com/javascript/jsapi/infotemplate-amd.html InfoTemplate}
+     * and binds it to a layer to provide "popup" information functionality when clicking on the visible layer in the map.
+     * This directive **must** be placed within an esri-feature-layer directive or esri-dynamic-map-service-layer directive.
+     * 
+     * ## Examples
+     * - {@link ../#/examples/dynamic-map-service-layers Multiple Dynamic Map Service Layers}
+     *
+     * <pre>
+     *   <!-- The title is provided as an attribute parameter
+     *   but the content is the entire inner HTML -->
+     *
+     *   <esri-info-template title="Parks">
+     *      <span>${PARK_NAME}</span>
+     *      <span>This park had ${TOTAL_VISITS_2014} visitors in 2014</span>
+     *   </esri-info-template>
+     * </pre>
+     *
+     * @param {String=} layer-id The layer id to which the InfoTemplate should be connected to.
+     *  **Note:** This is only applicable when the parent directive is an esri-dynamic-map-service-layer.
+     * @param {String=} title The title of the template.
+     * @param {String | Node[]=} content **Note:** The content of the template is provided as inner HTML to this directive.
+     * 
+     */
     angular.module('esri.map').directive('esriInfoTemplate', function () {
         // this object will tell angular how our directive behaves
         return {
