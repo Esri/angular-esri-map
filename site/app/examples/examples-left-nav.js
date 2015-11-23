e6b078b2e3b6c1c0f2a6b7d4fa34ad2714c89d2c
Jacob Wasilkowski <jwasilkowski@esri.com>
2015-11-23 07:17:18 -0600

    fine-tuning bootstrap grid column sizes for responsive left nav as well as map+legend examples; added initial display:none style for the left nav and to snippets tabs to clean up flashing startup appearance on the Home page

--- site/app/examples/examples-left-nav.js
+++ site/app/examples/examples-left-nav.js
@@ -4,13 +4,16 @@ angular.module('esri-map-docs')
     .controller('ExamplesLeftNavCtrl', function($scope, pathService, appConfig) {
 
         $scope.activeExample = null;
+        $scope.leftNavStyle = 'none';
 
         $scope.examplePageCategories = appConfig.examplePageCategories;
 
         // toggle left nav visibility based on route path
         // toggle 'active' class of list items based on route controller name
         $scope.$on('$routeChangeStart', function(event, next) {
-            $scope.leftNavShow = (pathService.getPathParts(next.originalPath)[0] === 'examples');
+            var isExamplePath = (pathService.getPathParts(next.originalPath)[0] === 'examples');
+            $scope.leftNavShow = isExamplePath;
+            $scope.leftNavStyle = isExamplePath ? 'block' : 'none';
             $scope.activeExample = next.controller;
         });
     });
