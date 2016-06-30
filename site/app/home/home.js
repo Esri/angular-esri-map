9a9ed5bb9b286fa31a33d31a5ab14817196da6c0
Jacob Wasilkowski <jwasilkowski@esri.com>
2016-06-30 16:01:49 -0500

    added animated titles for esri-map-view and esri-scene-view in jumbotron

--- site/app/home/home.js
+++ site/app/home/home.js
@@ -1,5 +1,15 @@
 'use strict';
 
 angular.module('esri-map-docs')
-    .controller('HomeCtrl', function() {
+    .controller('HomeCtrl', function($scope, $timeout) {
+        $scope.esriMapViewTitle = true;
+
+        var toggleJumbotronTitles = function() {
+            $timeout(function() {
+                $scope.esriMapViewTitle = $scope.esriMapViewTitle ? false : true;
+                toggleJumbotronTitles();
+            }, 5000);
+        };
+
+        toggleJumbotronTitles();
     });
